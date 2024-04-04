3869580ace9344ae54550bebf87736f2c5769887
sujal-shukla <shuklasujal714@gmail.com>
2024-04-04 21:11:11 +0530

    hero section done

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,8 +1,10 @@
-"use client"
+"use client";
+import { useEffect, useRef } from "react";
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
-import gsap from 'gsap'
-import { useEffect , useRef } from "react";
+import { Shapes } from "@/slices/Hero/Shapes";
+import Bounded from "@/components/Bounded";
+import gsap from "gsap";
 
 /**
  * Props for `Hero`.
@@ -13,7 +15,8 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
-  const component = useRef(null);  
+  const component = useRef(null);
+
   useEffect(() => {
     let ctx = gsap.context(() => {
       // create as many GSAP animations and/or ScrollTriggers here as you want...
@@ -30,9 +33,9 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
             ease: "elastic.out(1,0.3)",
             duration: 1,
             transformOrigin: "left top",
-            stagger: { each: 0.1, 
-              // from: "random" 
-            },
+            stagger: { each: 0.1,
+              //  from: "random" 
+              },
           },
         )
         .fromTo(
@@ -47,15 +50,13 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
             y: 0,
             duration: 1,
             scale: 1,
-            ease: "elastic.out(0.3,0.0)",
+            ease: "elastic.out(1,0.3)",
           },
         );
     }, component);
     return () => ctx.revert(); // cleanup!
   }, []);
 
-
- 
   const renderLetters = (name: KeyTextField, key: string) => {
     if (!name) return;
     return name.split("").map((letter, index) => (
@@ -67,22 +68,35 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       </span>
     ));
   };
+
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       ref={component}
     >
-      <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
-        <div className="col-start-1 lg:row-start-1">
-          <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter" aria-label={slice.primary.first_name + " " + slice.primary.last_name}>
-          <span className="block text-slate-300">{renderLetters(slice.primary.first_name, "first")}</span>
-          <span className="-mt-[.2em] block text-slate-500">{renderLetters(slice.primary.last_name, "last")}</span>
+      <div className="grid min-h-[70vh] grid-cols-1 items-center md:grid-cols-2">
+        <Shapes />
+        <div className="col-start-1 md:row-start-1 " data-speed=".2">
+          <h1
+            className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter"
+            aria-label={
+              slice.primary.first_name + " " + slice.primary.last_name
+            }
+          >
+            <span className="block text-slate-300 ">
+              {renderLetters(slice.primary.first_name, "first")}
+            </span>
+            <span className="-mt-[.2em] block text-slate-500  ">
+              {renderLetters(slice.primary.last_name, "last")}
+            </span>
           </h1>
-          <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-100 md:text-4xl">{slice.primary.tag_line}</span>
+          <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">
+            {slice.primary.tag_line}
+          </span>
         </div>
       </div>
-    </section>
+    </Bounded>
   );
 };
 
