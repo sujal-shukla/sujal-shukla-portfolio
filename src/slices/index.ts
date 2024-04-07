3298b99d1b62903a8645ba94254207d542578b35
sujal-shukla <shuklasujal714@gmail.com>
2024-04-07 22:57:57 +0530

    DONE

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -3,5 +3,10 @@
 import dynamic from "next/dynamic";
 
 export const components = {
+  biography: dynamic(() => import("./Biography")),
+  content_index: dynamic(() => import("./ContentIndex")),
   hero: dynamic(() => import("./Hero")),
+  image_block: dynamic(() => import("./ImageBlock")),
+  tech_list: dynamic(() => import("./TechList")),
+  textblock: dynamic(() => import("./Textblock")),
 };
